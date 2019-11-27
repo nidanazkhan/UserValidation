d028f0eb29d4d54fa39b4a7a0739c7136d7bebf3
nidanazkhan <khannd@mail.uc.edu>
2019-11-27 18:02:06 -0500

    Committing Individual Assignment proj

---
+++ UserValidation_first/UserValidation_first/Pages/ConsumeXML.cshtml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UserValidation_first.Pages
+{
+    public class ConsumeXMLModel : PageModel
+    {
+        private IHostingEnvironment _environment;
+        private string result;
+        public ConsumeXMLModel(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        [BindProperty]
+        public IFormFile Upload { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            string fileName = Upload.FileName;
+            string file = Path.Combine(_environment.ContentRootPath, "uploads", fileName);
+            using (var fileStream = new FileStream(file, FileMode.Create))
+            {
+                Upload.CopyTo(fileStream);
+            }
+            XmlDocument doc = new XmlDocument();
+            doc.Load(file);
+            XmlNode node = doc.SelectSingleNode("/users/user[firstname='Nikita']");
+           
+            ValidateXML(file);
+        }
+
+        private void ValidateXML(string file)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.Schema;
+
+            var xsdPath = Path.Combine(_environment.ContentRootPath, "uploads", "uservalidation(1).xsd");
+            settings.Schemas.Add(null, xsdPath);
+
+            settings.ValidationFlags |= System.Xml.Schema.XmlSchemaValidationFlags.ProcessSchemaLocation;
+            settings.ValidationFlags |= System.Xml.Schema.XmlSchemaValidationFlags.ReportValidationWarnings;
+            settings.ValidationEventHandler += new System.Xml.Schema.ValidationEventHandler(this.ValidationEventHandler);
+
+            XmlReader xmlReader = XmlReader.Create(file, settings);
+            try
+            {
+                while (xmlReader.Read())
+                {
+
+                }
+                result = "validation passed!";
+                ViewData["result"] = "validation Passed";
+            } catch(Exception e)
+            {
+                ViewData["result"] = e.Message;
+            }
+        }
+
+        public void ValidationEventHandler(object sender, ValidationEventArgs args)
+        {
+            result = "Validation failed. Message" + args.Message;
+            throw new Exception("Validation failed. Message" + args.Message);
+        }
+    }
+}
---
+++ UserValidation_first/UserValidation_first/wwwroot/uservalidation.xsl
@@ -0,0 +1,40 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:template match="/">
+    <html>
+      <head>
+        <meta charset="utf-8"/>
+        <title>Users</title>
+      </head>
+      <body>
+        <div>
+          <h1>Fav Users</h1>
+          <table border="1" width="100%">
+            <tr>
+              <th>firstname</th>
+              <th>lastname</th>
+              <th>bearcatid</th>
+              <th>favoriteshortcut key</th>
+            </tr>
+            <xsl:for-each select="/users/user">
+              <tr>
+                <td>
+                  <xsl:value-of select="firstname"/>
+                </td>
+                <td>
+                  <xsl:value-of select="lastname"/>
+                </td>
+                <td>
+                  <xsl:value-of select="bearcatid"/>
+                </td>
+                <td>
+                  <xsl:value-of select="favoriteshortcut"/>
+                </td>
+              </tr>
+            </xsl:for-each>
+          </table>
+        </div>
+      </body>
+    </html>
+  </xsl:template>
+</xsl:stylesheet>
